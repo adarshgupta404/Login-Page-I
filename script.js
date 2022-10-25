9c8db0c5eb3aa285f8750f19849b5a2cfc668524
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2022-10-26 00:40:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-$(".incontainer").on('mouseover', function () {
+$(".incontainer").on('mouseenter click', function () {
     $(".ro1").addClass("rot1");
     $(".ro2").addClass("rot2");
+    $(".rot1").css("transform", "rotate(15deg)");
+    $(".rot2").css("transform", "rotate(-15deg)");
+})
+
+$(".incontainer").on('mouseleave', function () {
+    $(".rot1").css("transform", "rotate(0deg)");
+    $(".rot2").css("transform", "rotate(0deg)");
 })
